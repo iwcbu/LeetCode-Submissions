2048c1279e9f73d7d6d75fe5139a07ccc6338964
Ian Campbell <iwc3@bu.edu>
2024-08-02 08:22:13 +0000

    [LeetCode Sync] Runtime - 11 ms (4.78%), Memory - 11.6 MB (100.00%)

---
+++ my-folder/0392-is-subsequence/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        x is the index of s
+        y is the index of t
+        """
+
+        x = 0
+        y = 0
+
+        while x < len(s) and y < len(t):
+            if s[x] == t[y]:
+                x += 1
+            y += 1  
+
+        return x == len(s)
