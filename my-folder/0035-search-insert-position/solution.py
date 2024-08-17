2ec3df1c9348b6af9e3e6789ee02eb57630a033f
Ian Campbell <iwc3@bu.edu>
2024-08-17 20:37:59 +0000

    [LeetCode Sync] Runtime - 282 ms (1.47%), Memory - 174.8 MB (1.28%)

--- my-folder/0035-search-insert-position/solution.py
+++ my-folder/0035-search-insert-position/solution.py
@@ -1,10 +1,13 @@
 class Solution(object):
     def searchInsert(self, nums, target):
-        if target not in nums:
-            if target < nums[0]:
-                return 0
-            if target > nums[-1]:
-                return len(nums)
-        for i in range(len(nums)):
-            if nums[i] == target or target < nums[i]:
-                return i
+        if len(nums) == 0:
+            return 0
+            
+        if nums[0] == target:
+            return 0
+            
+        if nums[0] < target:
+            return 1 + self.searchInsert(nums[1:], target)
+            
+        if nums[0] > target:
+            return 0
