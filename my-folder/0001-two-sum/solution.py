39f4fda278412d5b782373cd9cc20a94a9354f84
Ian Campbell <iwc3@bu.edu>
2024-08-19 09:35:09 +0000

    [LeetCode Sync] Runtime - 44 ms (45.34%), Memory - 12.5 MB (99.96%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,13 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
         hashmap = {}
-
         for i in range(len(nums)):
             p = target - nums[i]
             if p in hashmap:
                 return [hashmap[p], i]
-                
             hashmap[nums[i]] = i
-
-
-        
