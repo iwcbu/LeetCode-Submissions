d9db48b2919c20a8c0959073ed035aa8bb9d10e4
Ian Campbell <iwc3@bu.edu>
2024-10-17 12:38:39 +0000

    [LeetCode Sync] Runtime - 1 ms (95.68%), Memory - 11.5 MB (100.00%)

--- my-folder/0009-palindrome-number/solution.py
+++ my-folder/0009-palindrome-number/solution.py
@@ -1,12 +1,10 @@
 class Solution(object):
     def isPalindrome(self, x):
-        str_x = str(x)
-        half_x = len(str_x) / 2
-        for i in range(half_x):
-            j = i + 1
-            if str_x[i] != str_x[-j]:
-                return False
-
-        return True
-
-
+        """
+        :type x: int
+        :rtype: bool
+        """
+        string = str(x)
+        return string == string[::-1]
+        
+        
