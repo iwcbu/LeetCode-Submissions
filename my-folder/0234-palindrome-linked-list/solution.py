4b9880d3ff527101d0e7f661a006f55f2949c8d3
Ian Campbell <iwc3@bu.edu>
2026-01-06 00:41:43 +0000

    [LeetCode Sync] Runtime - 143 ms (23.51%), Memory - 83.6 MB (37.49%)

--- my-folder/0234-palindrome-linked-list/solution.py
+++ my-folder/0234-palindrome-linked-list/solution.py
@@ -9,27 +9,44 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: bool
         """
-        
 
-        arr = []
-        cur = head
-        
-        while cur != None:
-            arr.append(cur.val)
-            cur = cur.next
-        
-        lenarr = len(arr)
-
-        if lenarr <= 1:
+        if head.next == None:
             return True
+        
+    
+        stack = []
+        cur1 = head
+        cur2 = head
+        lenlist = 0
 
-        for i in range(lenarr):
-            noti = (i+1) * -1
-            if arr[i] != arr[noti]:
+        while cur1 and cur2:
+            stack.append(cur1.val)
+            cur1 = cur1.next
+            cur2 = cur2.next
+            lenlist += 1
+            if cur2 == None:
+                break
+            cur2 = cur2.next
+            lenlist += 1
+        
+        lenstack = len(stack)
+        if lenstack < 2:
+            val = stack.pop()
+            if cur1.val == val:
+                return True
+            else:
                 return False
 
+        if lenlist % 2 == 1:
+            stack.pop()
+        
+        while cur1:
+            val = stack.pop()
+            if val != cur1.val:
+                return False
+            cur1 = cur1.next
+        
         return True
-
         
 
 
